f4d73450b21c01e6b7eedd064d7586ffa2d9938a
yogeswar142 <amarayogeswararao16@email.com>
2026-04-13 23:59:22 +0530

    chore: add .gitignore and cleanup repository

--- node_modules/got/dist/source/core/utils/defer-to-connect.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Socket } from 'node:net';
-import type { TLSSocket } from 'node:tls';
-type Listeners = {
-    connect?: () => void;
-    secureConnect?: () => void;
-    close?: (hadError: boolean) => void;
-};
-declare const deferToConnect: (socket: TLSSocket | Socket, fn: Listeners | (() => void)) => void;
-export default deferToConnect;
